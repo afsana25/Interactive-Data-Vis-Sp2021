abc26e4281978d0585a2d9ec25e9a0196e0a8580
afsana25 <afsana2012mimi5786@gmail.com>
2021-03-01 02:12:30 -0500

    Scatter Plots Have Created

--- tutorial3_distributions/main.js
+++ tutorial3_distributions/main.js
@@ -6,69 +6,130 @@ const width = window.innerWidth * 0.7,
 
 // these variables allow us to access anything we manipulate in init() but need access to in draw().
 // All these variables are empty before we assign something to them.
-let svg;
 let xScale;
 let yScale;
 
+let svg1;
+
 /* APPLICATION STATE */
-let state = {
-  data: [],
-  selection: "All" // + YOUR FILTER SELECTION
+let state = { //state is just an object keep track of user selection
+  data: [], // one of the property data, it will keep track of whole data sets
+  selectedParty: "All" // + YOUR FILTER SELECTION
 };
 
 /* LOAD DATA */
-d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
+d3.json('../data/environmentRatings.json', d3.autoType).then(raw_data => {
   // + SET YOUR DATA PATH
   console.log("raw_data", raw_data);
-  state.data = raw_data;
-  init();
+  state.data = raw_data; //save our data to application state
+init(); //initializing function
 });
 
 /* INITIALIZING FUNCTION */
 // this will be run *one time* when the data finishes loading in 
 function init() {
+
+  console.log('state', state)
+
   // + SCALES
+ xScale = d3.scaleLinear()
+  .domain(d3.extent(state.data, d=>d.ideologyScore2020)) //state.data= holding our data
+  //
+.range([margin.left, width - margin.right])
 
-  // + AXES
+  console.log("xScale", xScale, xScale(0.8417651235))
 
-  // + UI ELEMENT SETUP
-
-  const selectElement = d3.select("#dropdown").on("change", function() {
-    // `this` === the selectElement
-    // 'this.value' holds the dropdown value a user just selected
-
-    state.selection = this.value
-    console.log("new value is", this.value);
-    draw(); // re-draw the graph based on this new selection
-  });
-
-  // add in dropdown options from the unique values in the data
-  selectElement
-    .selectAll("option")
-    .data(["All", "1", "2", "3"]) // + ADD UNIQUE VALUES
-    .join("option")
-    .attr("value", d => d)
-    .text(d => d);
-
-  // + CREATE SVG ELEMENT
-
-  // + CALL AXES
-
-  draw(); // calls the draw function
-}
-
-/* DRAW FUNCTION */
- // we call this everytime there is an update to the data/state
-function draw() {
+yScale = d3.scaleLinear()
+ .domain(d3.extent(state.data, d=>d.envScore2020) )// [min, max]
+  .range([height-margin.bottom, margin.top]);
   
-  // + FILTER DATA BASED ON STATE
+  console.log("yScale", yScale, yScale(7))
+  // + AXES
+const xAxis  = d3.axisBottom(xScale)
+const yAxis  = d3.axisLeft(yScale)
 
-  // const dot = svg
-  //   .selectAll("circle")
-  //   .data(filteredData, d => d.name)
-  //   .join(
-  //     enter => enter, // + HANDLE ENTER SELECTION
-  //     update => update, // + HANDLE UPDATE SELECTION
-  //     exit => exit // + HANDLE EXIT SELECTION
-  //   );
+//Create svg
+svg1 = d3.selectAll('#d3-container')
+.append("svg")
+.attr('width', width)
+.attr('height', height)
+
+// already set transform to (x,y)=(0, 240px)
+svg1.append("g")
+.attr("class", "xAxis")
+.attr("transform", `translate(${0}, ${height-margin.bottom})`) //translate(x,y)
+.call(xAxis)
+.append("text")
+.text("ideologyScore2020")
+.attr("transform", `translate(${width/2}, ${40})`)
+
+svg1.append("g")
+.attr("class", "yAxis") 
+.attr("transform", `translate(${margin.left},${0})`)//translate(x,y)
+.call(yAxis)
+
+//SETUP UI ELEMENTS
+
+const dropdown1 = d3.select("#dropdown1")
+dropdown1.selectAll("options")
+.data(["All", "R", "D"])
+.join("option")
+.attr("value", d=>d)
+.text(d=>d)
+
+dropdown1.on("change", event => {
+console.log("dropdown changed!", event.target.value) 
+state.selectedParty = event.target.value
+console.log("new state:", state)
+draw(); //Recall draw
+})
+
+draw();
+
 }
+
+function draw(){
+  console.log("I am in the draw function!")
+
+  //Filter data besed on state
+
+const filteredData = state.data
+.filter(d=> {
+  if (state.selectedParty=== "All") return true
+ else return  d.Party === state.selectedParty
+})
+
+// +DRAW Circles
+
+svg1.selectAll("circle")
+  .data(filteredData, d=>d.BioID) // Array of object [{},{}]
+  .join(
+    enter=>enter.append("circle")
+  .attr("r", radius)
+  // set attribute before
+  .attr("cx", width- margin.right)
+  //.attr("fill", "magenta")
+  .attr("fill", i =>{
+  if (i.Party=== "R") return "red"
+  else return "blue"
+  })
+  )
+  // start a transition
+  .attr("cy", i=>yScale(i.envScore2020))
+  .call(enter=> enter.transition()
+  .duration(1000) // 1000 miliseconds
+  //set the end attribute
+  .attr("cx", i=>xScale(i.ideologyScore2020))
+  ) 
+//set the end attribute
+  ,
+  update=>update,
+  exit => exit
+  .call(exit
+   .transition()
+   .duration(1000)
+   .attr("cy", height)
+   .remove())
+
+}
+
