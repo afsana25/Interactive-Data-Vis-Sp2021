6aef80451aa8ff36a7036a6841c00b5c56ef3085
afsana25 <afsana2012mimi5786@gmail.com>
2021-04-24 03:55:24 -0400

    code

--- Project2_Narrative_Analysis/main.js
+++ Project2_Narrative_Analysis/main.js
@@ -1,43 +1,38 @@
-const width = window.innerWidth * 0.9,
-  height = window.innerHeight * 0.7,
-  margin = { top: 20, bottom: 50, left: 60, right: 40 };
+// Load external data and boot
+d3.json("./Data/world.json", d3.autoType).then( data=>{
+ console.log("data", data)
 
-/** these variables allow us to access anything we manipulate in
- * init() but need access to in draw().
- * All these variables are empty before we assign something to them.*/
-let svg;
+ // The svg
+ //svg = d3.select("svg"),
 
-/**
- * APPLICATION STATE
- * */
-let state = {
-  // + SET UP STATE
-  geojson: null,
-  population: null,
-  hover: {
-    screenPosition: null, // will be array of [x,y] once mouse is hovered on something
-     mapPosition: null, // will be array of [long, lat] once mouse is hovered on something
-    SName : null,
-    Stemp: null,
-    visible: false,
-  }
-};
+  width = window.innerWidth * 0.8,
+  height = window.innerHeight * 0.8,
+  margin = { top: 20, bottom: 50, left: 20, right: 40 }
+  
+ svg = d3.select("#d3-container")
+ .append("svg")
+ .attr("width", width)
+ .attr("height", height)
 
-/**
- * LOAD DATA
- * Using a Promise.all([]), we can load more than one dataset at a time
- * */
-Promise.all([
-  d3.json("./Data/countries-50m.json"),
-  d3.csv("./Data/population.csv", d3.autoType),
-]).then(([geojson, Population]) => {
-  // + SET STATE WITH DATA
-  state.geojson = geojson
-  state.Population = Population
-  console.log("state: ", state);
-  init();
-});
+ 
 
-function init(){
+// Map and projection
+projection = d3.geoEqualEarth()
+    .fitSize([width, height], data)
 
-}
+    pathFunction = d3.geoPath()
+                .projection(projection)
+
+
+
+    // Draw the map
+    svg.append("g")
+        .selectAll("path")
+        .data(data.features)
+        .enter().append("path")
+            .attr("fill", "#69b3a2")
+            .attr("d", pathFunction)
+            .style("stroke", "#fff")
+})
+
+
